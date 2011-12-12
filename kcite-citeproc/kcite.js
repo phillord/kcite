216406d3d4117c0d9ad5d862f587812a395d5d0b
Phillip Lord <phillip.lord@newcastle.ac.uk>
2011-12-12 11:21:23 +0000

    Updated for 1.4 release

--- kcite-citeproc/kcite.js
+++ kcite-citeproc/kcite.js
@@ -1,3 +1,23 @@
+/*
+
+   Copyright (c) 2011.
+   Phillip Lord (phillip.lord@newcastle.ac.uk) and
+   Newcastle University. 
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 // modify the HTML output format so that the bibliography hyperlinks
 CSL.Output.Formats.kcite = CSL.Output.Formats.html;
--- kcite.php
+++ kcite.php
@@ -8,9 +8,23 @@
    Author URI: http://knowledgeblog.org
    Email: knowledgeblog-discuss@knowledgeblog.org
    
-   Copyright 2010. Simon Cockell (s.j.cockell@newcastle.ac.uk)
+   Copyright (c) 2010. Simon Cockell (s.j.cockell@newcastle.ac.uk)
    Phillip Lord (phillip.lord@newcastle.ac.uk)
    Newcastle University. 
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
    
   */
 
